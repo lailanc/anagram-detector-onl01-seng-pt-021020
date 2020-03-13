9f5b5bd03e1a8346ac8aeeec604721d826c85901
Laila Campbell <lailacampbell96@gmail.com>
2020-03-13 00:33:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ def initialize(word)
   @word = word
  end
  
- def match(array_words) 
+ def match (array_words) 
    array_words.select do |words| 
       words.split("").sort == @word.split("").sort
    end
