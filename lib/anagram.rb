c2742f81b4c643b0e998bd051d2a00fcb452f42e
Laila Campbell <lailacampbell96@gmail.com>
2020-03-13 00:49:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   
   def match (array_words)
   array_words.select do |words|
-  words.split ("").sort == @word.split ("").sort
+  words.split("").sort == @word.split("").sort
 end 
     
 end 
